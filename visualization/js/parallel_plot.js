35e04438cae687974c08bb92d5d2f52c65d50abc
Marco Morella <marcomorella1@gmail.com>
2020-05-09 19:40:56 +0200

    Initialized parallel plot for most of the features

--- visualization/js/parallel_plot.js
+++ visualization/js/parallel_plot.js
@@ -2,7 +2,7 @@ class ParallelPlot {
     constructor(dataUpdater) {
         this.dataUpdater = dataUpdater;
 
-        var margin = { top: 20, right: 5, bottom: 5, left: 25 }
+        var margin = { top: 20, right: 5, bottom: 5, left: 30 }
         var height = 300;
         var width = 800;
         this.height = height;
@@ -16,38 +16,49 @@ class ParallelPlot {
                   "translate(" + margin.left + "," + margin.top + ")");
 
 
-        //in the function below (on every callback in general) use referenceParallelPlot
-        //when you need to use 'this' (why? because the callback doesn't see 'this'... JS SHIT)
-        //so instead of using:
-        //this.svg = robe
-        //use:
-        //referenceParallelPlot.svg = robe
         var referenceParallelPlot = this;
         this.dataUpdater.addListener('dataReady', function(e) {
-
             referenceParallelPlot.startVisualization(referenceParallelPlot);
-
         });
     }
 
     startVisualization(referenceParallelPlot) {
         //to see the data(you can delete this line, it's only an example):
-        //console.log(referenceParallelPlot.dataUpdater.data); //TO DELETE
-        var dimensions = Object.keys(dataUpdater.data[0]).filter(function(d) { return d != "Current Version" && d !="Genres" && d !="Current Ver" && d != "comp0" && d!= "comp1" && d!="App" && d!="Type"});
-        //Yeah, i finished this hell-borne thing up here and noticed that this can be simply done with a list..
-
+        var dimensions = Object.keys(dataUpdater.data[0]).filter(function(d) { return d != "Current Version" && d !="Genres" && d !="Current Ver" && d != "comp0" && d!= "comp1" && d!="App" && d!="Type" && d!="LastUpdated"});
         var data = dataUpdater.data
         var i;
-
+        console.log(dataUpdater.data[0])
         var y = {}
+        //var c = {}
+        c["Category"] =  d3.map(data, function(d){return(d.Category)}).keys().sort()
+        c["ContentRating"] =  d3.map(data, function(d){return(d.ContentRating)}).keys().sort()
+
+        //DA RIVEDERE, this part was used to get all dates and put it in a domain list, due to the nature of the data set, it can't be done without
+        //modifying the csv first to comply with d3js
+
+        //c["LastUpdated"] =  d3.map(data, function(d){return(d.LastUpdated)}).keys().sort() //change to A
+/*
+        var counter = 0
+        var temp = []
+        c["LastUpdated"] = []
+        for (i in c["LastUpdatedA"]){
+          console.log(c["LastUpdatedA"][i])
+          if (counter%100 == 0){
+            temp.push(c["LastUpdatedA"][i])
+          }
+          counter = counter+1
+        }
+        c["LastUpdated"] = temp
+*/
+        //This is used to split between categorical and numerical values since this version of d3js uses respectively scalePOint and scaleLinear
+        //and we use the previously created domain lists to have their names on the axis.
         for (i in dimensions) {
           name = dimensions[i]
-          if (name == "Category"){
+          if (name == "Category" || name == "ContentRating"){// || name == "LastUpdated"){
             y[name] = d3.scalePoint()
-              .domain( d3.extent(data,function(d) { return d[name]; }) )
+              .domain(c[name])
               .range([0, referenceParallelPlot.height])
           }
-
           else{
             y[name] = d3.scaleLinear()
               .domain( d3.extent(data, function(d) { return +d[name]; }) )
@@ -85,8 +96,9 @@ class ParallelPlot {
           // I translate this element to its right position on the x axis
           .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
           // And I build the axis with the call function
-          .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
-          // Add axis title
+          .each(function(d) {
+            d3.select(this).call(d3.axisLeft().scale(y[d]));})
+          // Add axis titles
           .append("text")
             .style("text-anchor", "middle")
             .attr("y", -9)
