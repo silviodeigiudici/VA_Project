f2b08d91f9bb085af7d86b7007087a1ffdc9e48a
Marco Morella <marcomorella1@gmail.com>
2020-05-12 16:32:50 +0200

    Boxplots implemented, fixes needed on Review boxplot, next iteration should add labels to the boxplots

---
+++ visualization/js/boxplot1.js
@@ -0,0 +1,134 @@
+class BoxPlot1 {
+    constructor(dataUpdater) {
+        this.dataUpdater = dataUpdater;
+
+        var margin = { top: 15, right: 50, bottom: 20, left: 50 }
+        var height = 90;
+        var width = 30;
+        this.height = height;
+        this.width = width;
+        this.svg = d3.select(".boxplot1")
+          .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+        this.svg1 = d3.select(".boxplot2")
+          .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+        this.svg2 = d3.select(".boxplot3")
+          .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+
+        var referenceBoxPlot = this;
+        this.dataUpdater.addListener('dataReady', function(e) {
+            referenceBoxPlot.startVisualization(referenceBoxPlot);
+        });
+    }
+
+    startVisualization(referenceBoxPlot) {
+        //Let's make rate boxplot
+        var data_s = dataUpdater.data
+        var data_to_sort = []
+        var i
+        for (i in data_s){
+          if(data_s[i] != "NaN"){
+            data_to_sort.push(parseFloat(data_s[i].Rating))
+          }
+        }
+        data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
+        var data_sorted = data_to_sort.sort(d3.ascending)
+        makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg,data_sorted)
+
+        //Reviews boxplot
+        data_to_sort = []
+        for (i in data_s){
+          if(data_s[i].Reviews != "NaN"){
+            data_to_sort.push(parseInt(data_s[i].Reviews))
+          }
+        }
+        data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
+        data_sorted = data_to_sort.sort(d3.ascending)
+        //console.log(data_sorted)
+        makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg1,data_sorted)
+
+        //Size boxplot
+        data_to_sort = []
+        for (i in data_s){
+          if(data_s[i].Size != "NaN"){
+            data_to_sort.push(parseInt(data_s[i].Size))
+          }
+        }
+        data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
+        data_sorted = data_to_sort.sort(d3.ascending)
+        //console.log(data_sorted)
+        makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg2,data_sorted)
+      }
+    updateVisualization() {
+
+    }
+
+
+}
+function makeBoxPlot(referenceBoxPlot,svg,data_sorted){
+
+    // Compute summary statistics used for the box:
+    var q1 = d3.quantile(data_sorted, .25)
+    var median = d3.quantile(data_sorted, .5)
+    var q3 = d3.quantile(data_sorted, .75)
+    var interQuantileRange = q3 - q1
+    var min = data_sorted[0]
+    var max = data_sorted[data_sorted.length-1]
+    console.log(min, max)
+    // Show the Y scale
+    var y = d3.scaleLinear()
+      .domain([min,max])
+      .range([referenceBoxPlot.height, 0]);
+    svg.call(d3.axisLeft(y))
+
+
+    // a few features for the box
+    var center = 40
+    var width = 30
+
+    // Show the main vertical line
+    svg
+      .append("line")
+        .attr("x1", center)
+        .attr("x2", center)
+        .attr("y1", y(min) )
+        .attr("y2", y(max) )
+        .attr("stroke", "black")
+
+    // Show the box
+    svg
+      .append("rect")
+        .attr("x", center - width/2)
+        .attr("y", y(q3) )
+        .attr("height", (y(q1)-y(q3)) )
+        .attr("width", referenceBoxPlot.width )
+        .attr("stroke", "black")
+        .style("fill", "#69b3a2")
+
+    // show median, min and max horizontal lines
+    svg
+      .selectAll("toto")
+      .data([min, median, max])
+      .enter()
+      .append("line")
+        .attr("x1", center-referenceBoxPlot.width/2)
+        .attr("x2", center+referenceBoxPlot.width/2)
+        .attr("y1", function(d){ return(y(d))} )
+        .attr("y2", function(d){ return(y(d))} )
+        .attr("stroke", "black")
+}
--- visualization/js/init.js
+++ visualization/js/init.js
@@ -2,8 +2,9 @@
 var dataUpdater = new DataUpdater();
 
 var scatterplot = new Scatterplot(dataUpdater);
-var header = new Header(dataUpdater); 
+var header = new Header(dataUpdater);
 var parallelPlot = new ParallelPlot(dataUpdater);
+var boxPlot = new BoxPlot1(dataUpdater);
 
 
 dataUpdater.header = header;
