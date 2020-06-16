145c246d2dd061f8bbaca25a5819cd7f08a99112
Marco Morella <marcomorella1@gmail.com>
2020-06-16 20:50:06 +0200

    added side effect of brushing and paid/free selection on histograms

--- visualization/js/histo_cat.js
+++ visualization/js/histo_cat.js
@@ -6,9 +6,14 @@ class HistoCategory {
         var margin = { top: 50, right: 5, bottom: 10, left: 160 }
         var height = 350  ;
         var width = 330;
+        var x = {};
+        var y = {};
+        var dom = {};
+        this.dom = dom;
         this.height = height;
         this.width = width;
         this.margin = margin;
+        this.categories = []
         this.svg = d3.select(".barchart")
           .append("svg")
             .attr("width", '100%')
@@ -17,16 +22,42 @@ class HistoCategory {
             .attr("transform",
                   "translate(" + margin.left + "," + margin.top + ")");
 
-
         var referenceHistogramCat = this;
         this.dataUpdater.addListener('dataReady', function(e) {
             referenceHistogramCat.startVisualization(referenceHistogramCat);
+        });
+        this.dataUpdater.addListener('typeUpdateVisualization', function(e) {
+            referenceHistogramCat.updateVisualization(referenceHistogramCat);
         });
     }
 
     startVisualization(referenceHistogramCat) {
         //to see the data(you can delete this line, it's only an example):
-        var data = dataUpdater.data
+      var data = dataUpdater.data
+      var dataObj = referenceHistogramCat.dataObjCreation(data)
+      //console.log(dataObj)
+
+      //Making x and y axis
+      var y = d3.scaleBand()
+        .range([0, referenceHistogramCat.width])
+        .padding(0.1);
+      var x = d3.scaleLinear()
+                .range([0, referenceHistogramCat.height]);
+
+      // Scale the range of the data in the domains
+      var dom = dataObj.map(function(d) { return d.cat; })
+      y.domain(dom);
+      x.domain([0, d3.max(dataObj, function(d) { return d.frequencies; })]);
+      this.x = x;
+      this.y = y;
+      this.dom = dom;
+      referenceHistogramCat.buildVisualization(referenceHistogramCat,dataObj);
+
+      referenceHistogramCat.dataUpdater.addListener('brushParallelUpdateVisualization', function(e) {
+          referenceHistogramCat.updateVisualization(referenceHistogramCat);
+      });
+}
+    dataObjCreation(data){
         var i;
         var current;
         var occurances = {}
@@ -65,56 +96,81 @@ class HistoCategory {
         })
       }
       dataObj.sort((a, b) => (a.frequencies < b.frequencies) ? 1 : -1)
-      //console.log(dataObj)
-
-      //Making x and y axis
-      var y = d3.scaleBand()
-        .range([0, referenceHistogramCat.width])
-        .padding(0.1);
-      var x = d3.scaleLinear()
-                .range([0, referenceHistogramCat.height]);
-
-      // Scale the range of the data in the domains
-      y.domain(dataObj.map(function(d) { return d.cat; }));
-      x.domain([0, d3.max(dataObj, function(d) { return d.frequencies; })]);
-
-      // append the rectangles for the bar chart
-      referenceHistogramCat.svg.selectAll(".bar")
-          .data(dataObj)
-          .enter().append("rect")
-            .attr("class", "bar")
-            .attr("width", function(d) {return x(d.frequencies); } )
-            .attr("y", function(d) { return y(d.cat); })
-            .attr("height", y.bandwidth())
-            .attr("fill", "#69b3a2");
-
-      // drwa the x Axis
-      referenceHistogramCat.svg.append("g")
-          .call(d3.axisTop(x));
-
-      //label x Axis
-      referenceHistogramCat.svg.append("text")
-          .attr("transform",
-                "translate(" + ((referenceHistogramCat.width)/2) + " ," + "-" + referenceHistogramCat.margin.top/2 + ")")
-          .style("text-anchor", "middle")
-          .text("Apps");
-
-      // draw the y Axis
-      referenceHistogramCat.svg.append("g")
-          .call(d3.axisLeft(y));
-      //label y Axis
-      referenceHistogramCat.svg.append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 0 - referenceHistogramCat.margin.left)
-          .attr("x",0 - (referenceHistogramCat.height / 2))
-          .attr("dy", "1em")
-          .style("text-anchor", "middle")
-          .text("Category");
-
-}
-
-    updateVisualization() {
-
+      if (this.categories.length < categories.length){
+        this.categories = categories
+      }
+      return(dataObj)
     }
+  buildVisualization(referenceHistogramCat,dataObj){
+    var y = d3.scaleBand()
+      .range([0, referenceHistogramCat.width])
+      .padding(0.1);
+    var dom =  dataObj.map(function(d) { return d.cat; })
+
+    var i;
+    for (i in this.categories){
+      if (!dom.includes(this.categories[i])){
+        dom.push(this.categories[i]);
+      }
+    }
+    y.domain(dom)
+
+    var x = referenceHistogramCat.x;
+    // append the rectangles for the bar chart
+    referenceHistogramCat.svg.selectAll(".bar")
+        .data(dataObj)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("width", function(d) {return x(d.frequencies); } )
+          .attr("y", function(d) { return y(d.cat); })
+          .attr("height", y.bandwidth())
+          .attr("fill", "#69b3a2");
+
+    // drwa the x Axis
+    referenceHistogramCat.svg.append("g")
+        .call(d3.axisTop(x));
+
+    //label x Axis
+    referenceHistogramCat.svg.append("text")
+        .attr("transform",
+              "translate(" + ((referenceHistogramCat.width)/2) + " ," + "-" + referenceHistogramCat.margin.top/2 + ")")
+        .style("text-anchor", "middle")
+        .text("Apps");
+
+    // draw the y Axis
+    referenceHistogramCat.svg.append("g")
+        .call(d3.axisLeft(y));
+    //label y Axis
+    referenceHistogramCat.svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - referenceHistogramCat.margin.left)
+        .attr("x",0 - (referenceHistogramCat.height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Category");
+  }
+
+  updateVisualization(referenceHistogramCat) {
+    //here you need to update the visualization taking the new data from:
+    //referenceHistogram.dataUpdater.data
+    var margin = { top: 50, right: 5, bottom: 10, left: 160 }
+    var height = 350  ;
+    var width = 330;
+    this.height = height;
+    this.width = width;
+    d3.select(".barchart").select("svg").remove();
+    this.svg = d3.select(".barchart")
+      .append("svg")
+        .attr("width", '100%')
+        .attr("height", '100%')
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+    referenceHistogramCat = this;
+    var dataObj = referenceHistogramCat.dataObjCreation(referenceHistogramCat.dataUpdater.brushedData);
+
+    referenceHistogramCat.buildVisualization(referenceHistogramCat,dataObj);
+
+  }
 
 }
--- visualization/js/histo_content.js
+++ visualization/js/histo_content.js
@@ -9,6 +9,9 @@ class HistoContent {
         this.height = height;
         this.width = width;
         this.margin = margin;
+        var x = {};
+        var y = {};
+
         this.svg = d3.select(".histo_content")
           .append("svg")
             .attr("width", '100%')
@@ -17,52 +20,18 @@ class HistoContent {
             .attr("transform",
                   "translate(" + margin.left + "," + margin.top + ")");
 
-
         var referenceHistogram = this;
         this.dataUpdater.addListener('dataReady', function(e) {
             referenceHistogram.startVisualization(referenceHistogram);
+        });
+        this.dataUpdater.addListener('typeUpdateVisualization', function(e) {
+            referenceHistogram.updateVisualization(referenceHistogram);
         });
     }
 
     startVisualization(referenceHistogram) {
         //to see the data(you can delete this line, it's only an example):
-        var data = dataUpdater.data
-        var i;
-        var current;
-        var occurances = {}
-        var dat = []
-        var categories = []
-        for (i in data){
-          current = data[i].ContentRating
-          if (current == undefined){ //The dataset has some..imperfections
-            continue
-          }
-          if (occurances[current] == undefined){
-            occurances[current] = 1
-            categories.push(current)
-          }
-          else{
-            occurances[current] = occurances[current] + 1
-          }
-        }
-        var max = 0
-        var count = 0
-        for (i in occurances){
-          count = count +1
-          dat[i] = []
-          dat.push(occurances[i])
-          if (occurances[i] > max){
-            max = occurances[i]
-          }
-        }
 
-        var dataObj = []
-        for (let i = 0; i < count; i++) {
-        dataObj[i] = {
-          cat: categories[i],
-          frequencies: occurances[categories[i]]
-          }
-        }
 
         var x = d3.scaleBand()
                   .range([0, referenceHistogram.width])
@@ -70,60 +39,129 @@ class HistoContent {
         var y = d3.scaleLinear()
                   .range([referenceHistogram.height, 0]);
 
-
+        var dataObj = referenceHistogram.dataObjCreation(referenceHistogram.dataUpdater.data)
         // get the data
         // format the data
           // Scale the range of the data in the domains
         x.domain(dataObj.map(function(d) { return d.cat; }));
         y.domain([0, d3.max(dataObj, function(d) { return d.frequencies; })]);
+        this.x = x;
+        this.y = y;
 
+        referenceHistogram.buildVisualization(referenceHistogram,dataObj);
 
-        // append the rectangles for the bar chart
-        referenceHistogram.svg.selectAll(".bar")
-          .data(dataObj)
-          .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) { return x(d.cat); })
-            .attr("width", x.bandwidth())
-            .attr("y", function(d) { return y(d.frequencies); })
-            .attr("height", function(d) { return referenceHistogram.height - y(d.frequencies); })
-            .attr("fill", "#69b3a2");
-
-
-        /* #Label over bars, need fixing
-        referenceHistogram.svg.selectAll(".bar")
-        .append("text")
-          .style("text-anchor", "middle")
-          .attr("y", function(d) { return y(d.frequencies)+500; })
-          .attr("x", function(d) { return x(d.cat); })
-          .text(function(d) { return d.frequencies; })
-          .style("fill", "black")
-  */
-        // add the x Axis
-        referenceHistogram.svg.append("g")
-            .attr("transform", "translate(0," + referenceHistogram.height + ")")
-            .call(d3.axisBottom(x));
-        referenceHistogram.svg.append("text")
-            .attr("transform",
-                  "translate(" + ((referenceHistogram.width)/2) + " ,0" +
-                                 (referenceHistogram.height + 35) + ")")
-            .style("text-anchor", "middle")
-            .text("Content Rating");
-
-        // add the y Axis
-        referenceHistogram.svg.append("g")
-            .call(d3.axisLeft(y));
-        referenceHistogram.svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - referenceHistogram.margin.left)
-            .attr("x",0 - (referenceHistogram.height / 2))
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("Apps");
+        referenceHistogram.dataUpdater.addListener('brushParallelUpdateVisualization', function(e) {
+            referenceHistogram.updateVisualization(referenceHistogram);
+        });
 
     }
+    dataObjCreation(data){
+      var i;
+      var current;
+      var occurances = {}
+      var dat = []
+      var categories = []
+      for (i in data){
+        current = data[i].ContentRating
+        if (current == undefined){ //The dataset has some..imperfections
+          continue
+        }
+        if (occurances[current] == undefined){
+          occurances[current] = 1
+          categories.push(current)
+        }
+        else{
+          occurances[current] = occurances[current] + 1
+        }
+      }
+      var max = 0
+      var count = 0
+      for (i in occurances){
+        count = count +1
+        dat[i] = []
+        dat.push(occurances[i])
+        if (occurances[i] > max){
+          max = occurances[i]
+        }
+      }
 
-    updateVisualization() {
+      var dataObj = []
+      for (let i = 0; i < count; i++) {
+      dataObj[i] = {
+        cat: categories[i],
+        frequencies: occurances[categories[i]]
+        }
+      }
+      return(dataObj)
+    }
+    buildVisualization(referenceHistogram,dataObj){
+      var y = referenceHistogram.y;
+      var x = referenceHistogram.x;
+      // append the rectangles for the bar chart
+      referenceHistogram.svg.selectAll(".bar")
+        .data(dataObj)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.cat); })
+          .attr("width", x.bandwidth())
+          .attr("y", function(d) { return y(d.frequencies); })
+          .attr("height", function(d) { return referenceHistogram.height - y(d.frequencies); })
+          .attr("fill", "#69b3a2");
+
+      /* #Label over bars, need fixing
+      referenceHistogram.svg.selectAll(".bar")
+      .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", function(d) { return y(d.frequencies)+500; })
+        .attr("x", function(d) { return x(d.cat); })
+        .text(function(d) { return d.frequencies; })
+        .style("fill", "black")
+*/
+      // add the x Axis
+      referenceHistogram.svg.append("g")
+          .attr("transform", "translate(0," + referenceHistogram.height + ")")
+          .call(d3.axisBottom(x));
+      referenceHistogram.svg.append("text")
+          .attr("transform",
+                "translate(" + ((referenceHistogram.width)/2) + " ,0" +
+                               (referenceHistogram.height + 35) + ")")
+          .style("text-anchor", "middle")
+          .text("Content Rating");
+
+      // add the y Axis
+      referenceHistogram.svg.append("g")
+          .call(d3.axisLeft(y));
+      referenceHistogram.svg.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 0 - referenceHistogram.margin.left)
+          .attr("x",0 - (referenceHistogram.height / 2))
+          .attr("dy", "1em")
+          .style("text-anchor", "middle")
+          .text("Apps");
+
+    }
+    updateVisualization(referenceHistogram) {
+
+      //here you need to update the visualization taking the new data from:
+      //referenceHistogram.dataUpdater.data
+      var margin = referenceHistogram.margin;
+      var height = referenceHistogram.height;
+      var width = referenceHistogram.width;
+      this.height = height;
+      this.width = width;
+      d3.select(".histo_content").select("svg").remove();
+      referenceHistogram.svg = d3.select(".histo_content")
+        .append("svg")
+          .attr("width", '100%')
+          .attr("height", '100%')
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+      var dataObj = referenceHistogram.dataObjCreation(referenceHistogram.dataUpdater.brushedData);
+
+      referenceHistogram = this;
+      referenceHistogram.buildVisualization(referenceHistogram,dataObj);
+
 
     }
 
