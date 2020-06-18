d324c458853f2d7a7d7ffdc48079bd704c935a4f
Marco Morella <marcomorella1@gmail.com>
2020-06-18 15:41:15 +0200

    Boxplots are now interactive with brushing

--- visualization/js/boxplot1.js
+++ visualization/js/boxplot1.js
@@ -36,6 +36,13 @@ class BoxPlot1 {
         this.dataUpdater.addListener('dataReady', function(e) {
             referenceBoxPlot.startVisualization(referenceBoxPlot);
         });
+
+        this.dataUpdater.addListener('typeUpdateVisualization', function(e) {
+            referenceBoxPlot.updateVisualization(referenceBoxPlot);
+        });
+        referenceBoxPlot.dataUpdater.addListener('brushParallelUpdateVisualization', function(e) {
+            referenceBoxPlot.updateVisualization(referenceBoxPlot);
+        });
     }
 
     startVisualization(referenceBoxPlot) {
@@ -50,7 +57,10 @@ class BoxPlot1 {
         }
         data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
         var data_sorted = data_to_sort.sort(d3.ascending)
-        makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg,data_sorted,"Avg Rating")
+        var min = data_sorted[0]
+        var max = data_sorted[data_sorted.length-1]
+
+        referenceBoxPlot.makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg,data_sorted,"Avg Rating",min,max)
 
         //Reviews boxplot
         data_to_sort = []
@@ -61,8 +71,11 @@ class BoxPlot1 {
         }
         data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
         data_sorted = data_to_sort.sort(d3.ascending)
+        min = data_sorted[0]
+        max = data_sorted[data_sorted.length-1]
+
         //console.log(data_sorted)
-        makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg1,data_sorted, "#Reviews")
+        referenceBoxPlot.makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg1,data_sorted, "#Reviews",min,max)
 
         //Size boxplot
         data_to_sort = []
@@ -74,23 +87,104 @@ class BoxPlot1 {
         data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
         data_sorted = data_to_sort.sort(d3.ascending)
         //console.log(data_sorted)
-        makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg2,data_sorted,"Size")
+        min = data_sorted[0]
+        max = data_sorted[data_sorted.length-1]
+
+        referenceBoxPlot.makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg2,data_sorted,"Size",min,max)
       }
-    updateVisualization() {
+    updateVisualization(referenceBoxPlot) {
+      var margin = { top: 20, right: 50, bottom: 20, left: 90 }
+      var height = 110;
+      var width = 30;
+      this.height = height;
+      this.width = width;
+      this.margin = margin;
+
+      var data_s = referenceBoxPlot.dataUpdater.brushedData;
+      var data_to_sort = []
+      var i
+
+      d3.select(".boxplot1").select("svg").remove();
+
+      this.svg = d3.select(".boxplot1")
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+      for (i in data_s){
+        if(data_s[i] != "NaN"){
+          data_to_sort.push(parseFloat(data_s[i].Rating))
+        }
+      }
+      data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
+      var data_sorted = data_to_sort.sort(d3.ascending)
+      var min = data_sorted[0]
+      var max = data_sorted[data_sorted.length-1]
+
+      referenceBoxPlot.makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg,data_sorted,"Avg Rating",min,max)
+
+
+      d3.select(".boxplot2").select("svg").remove();
+
+      this.svg1 = d3.select(".boxplot2")
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+      //Reviews boxplot
+      data_to_sort = []
+      for (i in data_s){
+        if(data_s[i].Reviews != "NaN"){
+          data_to_sort.push(parseInt(data_s[i].Reviews))
+        }
+      }
+      data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
+      data_sorted = data_to_sort.sort(d3.ascending)
+      min = data_sorted[0]
+      max = data_sorted[data_sorted.length-1]
+
+      referenceBoxPlot.makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg1,data_sorted,"#Reviews",min,max)
+
+      d3.select(".boxplot3").select("svg").remove();
+
+      this.svg2 = d3.select(".boxplot3")
+        .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+      //Size boxplot
+      data_to_sort = []
+      for (i in data_s){
+        if(data_s[i].Size != "NaN"){
+          data_to_sort.push(parseInt(data_s[i].Size))
+        }
+      }
+      data_to_sort.pop() //One NaN is sneaking in somehow in the last loop..
+      data_sorted = data_to_sort.sort(d3.ascending)
+      //console.log(data_sorted)
+      min = data_sorted[0]
+      max = data_sorted[data_sorted.length-1]
+      referenceBoxPlot.makeBoxPlot(referenceBoxPlot,referenceBoxPlot.svg2,data_sorted,"Size",min,max)
 
     }
 
 
-}
-function makeBoxPlot(referenceBoxPlot,svg,data_sorted,axName){
+
+makeBoxPlot(referenceBoxPlot,svg,data_sorted,axName,min,max){
 
     // Compute summary statistics used for the box:
     var q1 = d3.quantile(data_sorted, .25)
     var median = d3.quantile(data_sorted, .5)
     var q3 = d3.quantile(data_sorted, .75)
     var interQuantileRange = q3 - q1
-    var min = data_sorted[0]
-    var max = data_sorted[data_sorted.length-1]
+
     // Show the Y scale
     var y = d3.scaleLinear()
       .domain([min,max])
@@ -140,4 +234,5 @@ function makeBoxPlot(referenceBoxPlot,svg,data_sorted,axName){
         .attr("y1", function(d){ return(y(d))} )
         .attr("y2", function(d){ return(y(d))} )
         .attr("stroke", "black")
+}
 }
